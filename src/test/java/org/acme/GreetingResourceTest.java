72b9169c07b05e1cfc6ef428e311438e58a00fb8
marouane <marwan.laarif94@gmail.com>
2022-04-20 06:04:54 +0000

    mocking works :D

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -1,8 +1,12 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -10,19 +14,18 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class GreetingResourceTest {
 
+    @InjectMock
+    GreetingConfiguration greetingConfiguration;
     @Test
     public void testHelloEndpoint() {
+        Mockito.when(greetingConfiguration.getName()).thenReturn(Optional.of("mocked"));
+        Mockito.when(greetingConfiguration.getMessage()).thenReturn("Salaaaam");
+
         given()
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
-
-    @Test
-    public void testConfiguration(){
-        GreetingService service= new GreetingService();
-        Assertions.assertEquals("Greetings dev", service.getGreeting());
+             .body(is("Salaaaam mocked"));
     }
 
 }
